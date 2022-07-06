4f9be398046608dc7961f17c1c69bcf897b84cbd
santiago.olmedo <santiago.olmedo@fing.edu.uy>
2022-07-06 00:01:42 -0300

    calcular_frecuencia

--- funciones_auxiliares.py
+++ funciones_auxiliares.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from itertools import islice, cycle
 
 # dado un dia de la semana retorna el tipo de dia:
 #    lunes a viernes: 1
@@ -13,11 +14,12 @@ def tipo_dia(fecha):
     else:
         return 1
 
+
 # retorna la desviacion de segundos entre el horario real y el teorico si aplica.
 # no aplica si no es el mismo tipo de dia o si hay mas de 900 segundos de diferencia
 
 
-def comparar_horarios(horario_real, horario_teorico, frecuencia):
+def comparar_horarios(horario_real, horario_teorico, frecuencia, margen):
     tipo_dia_teorico = horario_teorico[0]
     hora_teorica = horario_teorico[1]
     formato_fecha = "%Y-%m-%dT%H:%M:%S.%f%z"
@@ -25,24 +27,130 @@ def comparar_horarios(horario_real, horario_teorico, frecuencia):
     # caso borde entre tipos de dia
     if tipo_dia(fecha_completa.date()) != tipo_dia_teorico:
         return None
-    return comparar_horas(fecha_completa.time(), hora_teorica, frecuencia)
+    return comparar_horas(fecha_completa.time(), hora_teorica, frecuencia, margen)
 
 
-def comparar_horas(fecha_hora_real_string, hora_completa_teorica, frecuencia): 
-    desviacion = hora_a_segundos(
-         hora_int_a_datetime(hora_completa_teorica)) - hora_a_segundos(fecha_hora_real_string)
-    if desviacion > frecuencia*2/3 or desviacion < -frecuencia/3:
+def comparar_horas(fecha_hora_real_string, hora_completa_teorica, frecuencia, margen):
+    teo = hora_a_segundos(hora_int_a_datetime(hora_completa_teorica))
+    real = hora_a_segundos(fecha_hora_real_string)
+    if teo < real:
+        real = real - 86400
+    desviacion = teo - real
+    if desviacion > frecuencia * margen or desviacion < -frecuencia * (1 - margen):
         return None
     else:
         return desviacion
+
 
 def hora_int_a_datetime(hora_int):
     formato_hora = "%H:%M:%S"
     minutos_teoricos = int(hora_int % 100)
     hora_teorica = int((hora_int - minutos_teoricos) / 100)
-    return datetime.strptime(str(hora_teorica) + ":" + str(minutos_teoricos) + ":00", formato_hora).time()
-
+    return datetime.strptime(
+        str(hora_teorica) + ":" + str(minutos_teoricos) + ":00", formato_hora
+    ).time()
 
 
 def hora_a_segundos(hora_datetime):
-    return(hora_datetime.hour * 60 + hora_datetime.minute) * 60 + hora_datetime.second
+    return (hora_datetime.hour * 60 + hora_datetime.minute) * 60 + hora_datetime.second
+
+
+def calcular_promedio_diferencias(lista_horarios):
+    largo_lista_horarios = len(lista_horarios)
+    suma_diferencias = 0
+    for index in range(0, largo_lista_horarios - 1):
+        index_siguiente = index + 1
+        actual = hora_a_segundos(lista_horarios[index])
+        siguiente = hora_a_segundos(lista_horarios[index_siguiente])
+        if siguiente < actual:
+            actual = actual - 86400
+        suma_diferencias += abs(siguiente - actual)
+    return suma_diferencias / largo_lista_horarios
+
+
+def calcular_frecuencia(horario_teorico, index_horario, lista_horarios):
+    horarios_cercanos = []
+    largo_lista_horarios = len(lista_horarios)
+
+    cant_horarios_anteriores = 0
+    idx_anterior_horario = largo_lista_horarios - index_horario
+    cont = 0
+    for horario_anterior in islice(
+        cycle(reversed(lista_horarios)), idx_anterior_horario, None
+    ):
+        cont += 1
+        if horario_anterior[0] == horario_teorico[0]:
+            cant_horarios_anteriores += 1
+            horarios_cercanos.append(hora_int_a_datetime(horario_anterior[1]))
+            if cant_horarios_anteriores == 1 or cont == 10000:
+                break
+
+    horarios_cercanos.append(hora_int_a_datetime(horario_teorico[1]))
+
+    cant_horarios_posteriores = 0
+    cont = 0
+    for horario_posterior in islice(cycle(lista_horarios), index_horario + 1, None):
+        cont += 1
+        if horario_posterior[0] == horario_teorico[0]:
+            cant_horarios_posteriores += 1
+            horarios_cercanos.append(hora_int_a_datetime(horario_posterior[1]))
+        if cant_horarios_posteriores == 2 or cont == 10000:
+            break
+
+    index_eliminar = []
+
+    for index in range(0, len(horarios_cercanos) - 1):
+        index_siguiente = index + 1
+        actual = hora_a_segundos(horarios_cercanos[index])
+        siguiente = hora_a_segundos(horarios_cercanos[index_siguiente])
+        if siguiente < actual:
+            actual = actual - 86400
+        if abs(siguiente - actual) > 5400:
+            index_eliminar.append(index)
+
+    for index in reversed(index_eliminar):
+        del horarios_cercanos[index]
+
+    return calcular_promedio_diferencias(horarios_cercanos)
+
+
+def agregar_desviacion(
+    res_parcial_avenida,
+    nombre_avenida,
+    cod_parada,
+    cod_variante,
+    desviacion,
+    linea_empresa,
+):
+    if nombre_avenida in res_parcial_avenida:
+        if cod_parada in res_parcial_avenida[nombre_avenida]:
+            if cod_variante in res_parcial_avenida[nombre_avenida][cod_parada]:
+                res_parcial_avenida[nombre_avenida][cod_parada][cod_variante][0] = (
+                    res_parcial_avenida[nombre_avenida][cod_parada][cod_variante][0]
+                    * res_parcial_avenida[nombre_avenida][cod_parada][cod_variante][1]
+                    + desviacion
+                ) / (
+                    res_parcial_avenida[nombre_avenida][cod_parada][cod_variante][1] + 1
+                )
+                res_parcial_avenida[nombre_avenida][cod_parada][cod_variante][1] += 1
+            else:
+                res_parcial_avenida[nombre_avenida][cod_parada][cod_variante] = [
+                    desviacion,
+                    1,
+                    linea_empresa,
+                ]  # desviacion,cantidad,linea y empresa
+        else:
+            res_parcial_avenida[nombre_avenida][cod_parada] = {}
+            res_parcial_avenida[nombre_avenida][cod_parada][cod_variante] = [
+                desviacion,
+                1,
+                linea_empresa,
+            ]  # desviacion,cantidad,linea y empresa
+    else:
+        res_parcial_avenida[nombre_avenida] = {}
+        res_parcial_avenida[nombre_avenida][cod_parada] = {}
+        res_parcial_avenida[nombre_avenida][cod_parada][cod_variante] = [
+            desviacion,
+            1,
+            linea_empresa,
+        ]  # desviacion,cantidad,linea y empresa
--- procesarViajes.py
+++ procesarViajes.py
@@ -5,7 +5,7 @@ import pdb
 import funciones_auxiliares as func
 
 comienzo_tiempo = time.time()
-numero_de_procesos = 1
+numero_de_procesos = 4
 ruta_archivo_viajes = "datos/viajes/viajes_stm_052022.csv"
 ruta_archivo_horarios_teoricos = "datos/horariosOmnibus.csv"
 ruta_archivo_horarios_teoricos_circulares = "datos/horariosOmnibusCirculares.csv"
@@ -116,127 +116,51 @@ for _horario_teorico in horarios_teoricos:
 archivo_horarios_teoricos.close()
 del horarios_teoricos
 
-# # Recorrer los horarios teóricos circulares y quedarnos solo con aquellos que pertenezcan a las paradas relevantes
-# archivo_horarios_teoricos_circulares = open(ruta_archivo_horarios_teoricos_circulares)
-# horarios_teoricos_circulares = archivo_horarios_teoricos_circulares.readlines()
-# horarios_teoricos_circulares = horarios_teoricos_circulares[1:]
-# for _horario_teorico_circular in horarios_teoricos_circulares:
-#     horario_teorico = _horario_teorico_circular.split(";")
-#     for avenida in lista_paradas_avenida.items():
-#         paradas_por_avenida = avenida[1]
-#         for parada in paradas_por_avenida:
-#             cod_parada = int(horario_teorico[3])
-#             if cod_parada == parada[2]:
-#                 tipo_dia_teorico = int(horario_teorico[0])
-#                 cod_variante = int(horario_teorico[4])
-#                 horario_teorico = int(horario_teorico[7])
-#                 nombre_avenida = avenida[0]
-#                 if cod_parada in lista_horarios_teoricos_parada:
-#                     if cod_variante in lista_horarios_teoricos_parada[cod_parada]:
-#                         lista_horarios_teoricos_parada[cod_parada][cod_variante].append(
-#                             [tipo_dia_teorico, horario_teorico, nombre_avenida])
-#                     else:
-#                         lista_horarios_teoricos_parada[cod_parada][cod_variante] = [
-#                             [tipo_dia_teorico, horario_teorico, nombre_avenida]]
-#                 else:
-#                     lista_horarios_teoricos_parada[cod_parada] = {}
-#                     lista_horarios_teoricos_parada[cod_parada][cod_variante] = [
-#                         [tipo_dia_teorico, horario_teorico, nombre_avenida]]
-# archivo_horarios_teoricos.close()
-# del horarios_teoricos_circulares
-
-
-def agregar_desviacion(res_parcial_avenida, nombre_avenida, cod_parada, cod_variante, desviacion, linea_empresa):
-    if nombre_avenida in res_parcial_avenida:
-        if cod_parada in res_parcial_avenida[nombre_avenida]:
-            if (
-                cod_variante
-                in res_parcial_avenida[nombre_avenida][cod_parada]
-            ):
-                res_parcial_avenida[nombre_avenida][cod_parada][
-                    cod_variante
-                ][0] = (
-                    res_parcial_avenida[nombre_avenida][cod_parada][
-                        cod_variante
-                    ][0]
-                    * res_parcial_avenida[nombre_avenida][
-                        cod_parada
-                    ][cod_variante][1]
-                    + desviacion
-                ) / (
-                    res_parcial_avenida[nombre_avenida][cod_parada][
-                        cod_variante
-                    ][1]
-                    + 1
-                )
-                res_parcial_avenida[nombre_avenida][cod_parada][
-                    cod_variante
-                ][1] += 1
-            else:
-                res_parcial_avenida[nombre_avenida][cod_parada][
-                    cod_variante
-                ] = [
-                    desviacion,
-                    1,
-                    linea_empresa,
-                ]  # desviacion,cantidad,linea y empresa
-        else:
-            res_parcial_avenida[nombre_avenida][cod_parada] = {
-            }
-            res_parcial_avenida[nombre_avenida][cod_parada][
-                cod_variante
-            ] = [
-                desviacion,
-                1,
-                linea_empresa,
-            ]  # desviacion,cantidad,linea y empresa
-    else:
-        res_parcial_avenida[nombre_avenida] = {}
-        res_parcial_avenida[nombre_avenida][cod_parada] = {
-        }
-        res_parcial_avenida[nombre_avenida][cod_parada][
-            cod_variante
-        ] = [
-            desviacion,
-            1,
-            linea_empresa,
-        ]  # desviacion,cantidad,linea y empresa
-
-
-def calcular_frecuencia(horario_teorico, index_horario, lista_horarios):
-    print(lista_horarios)
-    print(horario_teorico)
-    print(index_horario)
-    cant_horarios_posteriores = 0
-    horarios_cercanos_posteriores = []
-    idx_proximo_horario = (index_horario+1) % len(lista_horarios)
-    for horario_posterior in lista_horarios[idx_proximo_horario:]:
-        if horario_posterior[0] == horario_teorico[0]:
-            cant_horarios_posteriores += 1
-            horarios_cercanos_posteriores.append(
-                func.hora_int_a_datetime(horario_posterior[1]))
-        if cant_horarios_posteriores == 2:
-            break
-
-    horarios_cercanos_anteriores = []
-    cant_horarios_anteriores = 0
-    idx_anterior_horario = ((index_horario-1) % len(lista_horarios)) + 1
-    for horario_anterior in reversed(lista_horarios[:idx_anterior_horario]):
-        if horario_anterior[0] == horario_teorico[0]:
-            cant_horarios_anteriores += 1
-            horarios_cercanos_anteriores.append(
-                func.hora_int_a_datetime(horario_anterior[1]))
-            if cant_horarios_anteriores == 1:
+# Recorrer los horarios teóricos circulares y quedarnos solo con aquellos que pertenezcan a las paradas relevantes
+archivo_horarios_teoricos_circulares = open(ruta_archivo_horarios_teoricos_circulares)
+horarios_teoricos_circulares = archivo_horarios_teoricos_circulares.readlines()
+horarios_teoricos_circulares = horarios_teoricos_circulares[1:]
+for _horario_teorico_circular in horarios_teoricos_circulares:
+    horario_teorico = _horario_teorico_circular.split(";")
+    for avenida in lista_paradas_avenida.items():
+        paradas_por_avenida = avenida[1]
+        for parada in paradas_por_avenida:
+            cod_parada = int(horario_teorico[3])
+            if cod_parada == parada[2]:
+                tipo_dia_teorico = int(horario_teorico[0])
+                cod_variante = int(horario_teorico[4])
+                horario_teorico = int(horario_teorico[7])
+                nombre_avenida = avenida[0]
+                if cod_parada in lista_horarios_teoricos_parada:
+                    if cod_variante in lista_horarios_teoricos_parada[cod_parada]:
+                        lista_horarios_teoricos_parada[cod_parada][cod_variante].append(
+                            [tipo_dia_teorico, horario_teorico, nombre_avenida]
+                        )
+                    else:
+                        lista_horarios_teoricos_parada[cod_parada][cod_variante] = [
+                            [tipo_dia_teorico, horario_teorico, nombre_avenida]
+                        ]
+                else:
+                    lista_horarios_teoricos_parada[cod_parada] = {}
+                    lista_horarios_teoricos_parada[cod_parada][cod_variante] = [
+                        [tipo_dia_teorico, horario_teorico, nombre_avenida]
+                    ]
                 break
+archivo_horarios_teoricos.close()
+del horarios_teoricos_circulares
 
-    print("cant_horarios_anteriores: ")
-    print(cant_horarios_anteriores)
-    print("cant_horarios_posteriores: ")
-    print(cant_horarios_posteriores)
-    print("horarios cercanos: ")
-    print(horarios_cercanos)
-
-    return
+for parada in lista_horarios_teoricos_parada:
+    for variante in lista_horarios_teoricos_parada[parada]:
+        lista_horarios_teoricos_parada[parada][variante].sort()
+        horarios_teoricos_ordenados = sorted(
+            lista_horarios_teoricos_parada[parada][variante],
+            key=lambda x: x[1],
+        )
+        for index_horario, horario_teorico in enumerate(horarios_teoricos_ordenados):
+            frecuencia = func.calcular_frecuencia(
+                horario_teorico, index_horario, horarios_teoricos_ordenados
+            )
+            horario_teorico.append(frecuencia)
 
 
 def calcular_desviacion(viajes, cola):
@@ -247,22 +171,27 @@ def calcular_desviacion(viajes, cola):
         cod_variante = int(viaje[16])
         if cod_parada in lista_horarios_teoricos_parada:
             if cod_variante in lista_horarios_teoricos_parada[cod_parada]:
-                lista_horarios_teoricos_parada[cod_parada][cod_variante].sort()
-                horarios_teoricos_ordenados = sorted(
-                    lista_horarios_teoricos_parada[cod_parada][cod_variante], key=lambda x: x[1])
-                for index_horario, horario_teorico in enumerate(horarios_teoricos_ordenados):
-                    frecuencia = calcular_frecuencia(
-                        horario_teorico, index_horario, horarios_teoricos_ordenados)
+                for horario_teorico in lista_horarios_teoricos_parada[cod_parada][
+                    cod_variante
+                ]:
+                    frecuencia = horario_teorico[3]
                     horario_real = viaje[2]
                     desviacion = func.comparar_horarios(
-                        horario_real, horario_teorico, frecuencia)
+                        horario_real, horario_teorico, frecuencia, 2 / 3
+                    )
                     if desviacion != None:
                         nombre_avenida = lista_horarios_teoricos_parada[cod_parada][
                             cod_variante
                         ][0][2]
                         linea_empresa = viaje[13] + " " + viaje[15]
-                        agregar_desviacion(res_parcial_avenida, nombre_avenida,
-                                           cod_parada, cod_variante, desviacion, linea_empresa)
+                        func.agregar_desviacion(
+                            res_parcial_avenida,
+                            nombre_avenida,
+                            cod_parada,
+                            cod_variante,
+                            desviacion,
+                            linea_empresa,
+                        )
     cola.put(res_parcial_avenida)
 
 
@@ -273,11 +202,13 @@ cant_viajes_por_proceso = int(len(viajes) / numero_de_procesos)
 procesos = []
 for nro_proceso in range(numero_de_procesos):
     comienzo = nro_proceso * cant_viajes_por_proceso
-    fin = comienzo + cant_viajes_por_proceso if nro_proceso != numero_de_procesos - \
-        1 else len(viajes)
+    fin = (
+        comienzo + cant_viajes_por_proceso
+        if nro_proceso != numero_de_procesos - 1
+        else len(viajes)
+    )
     procesos.append(
-        mp.Process(target=calcular_desviacion, args=(
-            viajes[comienzo:fin], cola_res))
+        mp.Process(target=calcular_desviacion, args=(viajes[comienzo:fin], cola_res))
     )
 
 for p in procesos:
