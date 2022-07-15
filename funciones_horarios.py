c66dd1e870375fbe0d28c24dac356c56a6369776
Santiago Guridi <santiago.guridi@cedarcode.com>
2022-07-15 00:01:19 -0400

    Refactor. agregados archivos funciones_horarios.py y funciones_viajes.py

---
+++ funciones_horarios.py
@@ -0,0 +1,99 @@
+import datetime
+
+UNA_HORA = 3600
+UN_DIA_EN_SEGUNDOS = 86400
+VEINTITRES_HORAS = 82800
+
+# dado un dia de la semana retorna el tipo de dia:
+#    lunes a viernes: 1
+#    sabado: 2
+#    domingo: 3
+
+
+def tipo_dia(fecha):
+    weekday = fecha.weekday()
+    if weekday == 5:
+        return 2
+    elif weekday == 6:
+        return 3
+    else:
+        return 1
+
+
+# retorna la desviacion de segundos entre el horario real y el teorico si aplica.
+# no aplica si no es el mismo tipo de dia o si hay mas de 900 segundos de diferencia
+
+
+def comparar_horarios(horario_real, horario_teorico, frecuencia, margen):
+    tipo_dia_teorico = horario_teorico[0]
+    hora_teorica = horario_teorico[1]
+    formato_fecha = "%Y-%m-%dT%H:%M:%S.%f%z"
+    fecha_completa = datetime.datetime.strptime(horario_real, formato_fecha)
+    # caso borde entre tipos de dia
+    if tipo_dia(fecha_completa.date()) != tipo_dia_teorico:
+        return None
+    return comparar_horas(fecha_completa.time(), hora_teorica, frecuencia, margen)
+
+
+def comparar_horas(fecha_hora_real_string, hora_completa_teorica, frecuencia, margen):
+    hora_teorica_en_segundos = hora_a_segundos(
+        hora_int_a_datetime(hora_completa_teorica)
+    )
+    hora_real_en_segundos = hora_a_segundos(fecha_hora_real_string)
+    if hora_real_en_segundos > VEINTITRES_HORAS and hora_teorica_en_segundos < UNA_HORA:
+        hora_teorica_en_segundos += UN_DIA_EN_SEGUNDOS
+    elif (
+        hora_teorica_en_segundos > VEINTITRES_HORAS and hora_real_en_segundos < UNA_HORA
+    ):
+        hora_real_en_segundos += UN_DIA_EN_SEGUNDOS
+    desviacion = hora_real_en_segundos - hora_teorica_en_segundos
+    if desviacion > frecuencia * margen or desviacion < -frecuencia * (1 - margen):
+        return None
+    else:
+        return desviacion
+
+
+def hora_int_a_datetime(hora_int):
+    formato_hora = "%H:%M:%S"
+    minutos_teoricos = int(hora_int % 100)
+    hora_teorica = int((hora_int - minutos_teoricos) / 100)
+    return datetime.datetime.strptime(
+        str(hora_teorica) + ":" + str(minutos_teoricos) + ":00", formato_hora
+    ).time()
+
+
+def hora_a_segundos(hora_datetime):
+    return (hora_datetime.hour * 60 + hora_datetime.minute) * 60 + hora_datetime.second
+
+
+def calcular_promedio_diferencias(lista_horarios):
+    largo_lista_horarios = len(lista_horarios)
+    suma_diferencias = 0
+    for index in range(0, largo_lista_horarios - 1):
+        index_siguiente = index + 1
+        actual = hora_a_segundos(lista_horarios[index])
+        siguiente = hora_a_segundos(lista_horarios[index_siguiente])
+        if siguiente < actual:
+            actual = actual - UN_DIA_EN_SEGUNDOS
+        suma_diferencias += abs(siguiente - actual)
+    return suma_diferencias / (largo_lista_horarios - 1)
+
+
+def obtener_dia_semana(horario_real):
+    formato_fecha = "%Y-%m-%dT%H:%M:%S.%f%z"
+    fecha_completa = datetime.datetime.strptime(horario_real, formato_fecha)
+    dia_numero = fecha_completa.date().weekday()
+    if dia_numero < 5:
+        return "Semana"
+    else:
+        return "Fin de Semana"
+
+
+def obtener_franja_horaria(horario_real):
+    formato_fecha = "%Y-%m-%dT%H:%M:%S.%f%z"
+    fecha_completa = datetime.datetime.strptime(horario_real, formato_fecha)
+    hora = fecha_completa.time().hour
+    if hora >= 8 and hora < 10 or hora >= 17 and hora < 19:
+        return "Horas pico 8-10 y 17-19"
+    else:
+        return "Horas tranquilas"
--- procesarViajes.py
+++ procesarViajes.py
@@ -1,266 +1,21 @@
-from ctypes.util import find_library
 import multiprocessing as mp
 import time
-import funciones_auxiliares as func
+import funciones_viajes as func_v
 
 NUMERO_DE_PROCESOS = 6
 RUTA_ARCHIVO_VIAJES = "datos/viajes/viajes_stm_052022.csv"
-RUTA_ARCHIVO_HORARIOS_TEORICOS = "datos/horariosOmnibus.csv"
-RUTA_ARCHIVO_HORARIOS_TEORICOS_CIRCULARES = "datos/horariosOmnibusCirculares.csv"
-RUTA_ARCHIVO_PARADAS = "datos/ubicacionParadas/paradas.csv"
-RUTA_ARCHIVO_AVENIDAS = "listaAvenidas"
-DEFAULT_MARGEN = 2 / 3
-
-
-def obtener_avenidas():
-    lista_avenidas = []
-    archivo_avenidas = open(RUTA_ARCHIVO_AVENIDAS)
-    filas_avenidas = archivo_avenidas.readlines()
-    filas_avenidas = filas_avenidas[1:]
-    for fila in filas_avenidas:
-        lista_avenidas.append(fila.rstrip("\n"))
-    archivo_avenidas.close()
-    return lista_avenidas
-
-
-def obtener_paradas_avenida(lista_avenidas):
-    # key = codigo_parada, values = [contador_viajes,calle 1, calle 2]
-    lista_paradas_contador = {}
-
-    # Recorremos las paradas y nos quedamos con las que pertenecen a las calles elegidas
-    archivo_paradas = open(RUTA_ARCHIVO_PARADAS)
-    paradas = archivo_paradas.readlines()
-    paradas = paradas[1:]
-    for _parada in paradas:
-        parada = _parada.split(",")
-        nombre_avenida = parada[4]
-        calle_2 = parada[5]
-        if nombre_avenida in lista_avenidas:
-            lista_paradas_contador[int(parada[0])] = [0, nombre_avenida, calle_2]
-    archivo_paradas.close()
-
-    # Recorremos los viajes y le sumamos uno al contador de cada parada
-    archivo_viajes = open(RUTA_ARCHIVO_VIAJES)
-    viajes = archivo_viajes.readlines()
-    viajes = viajes[1:]
-    for _viaje in viajes:
-        viaje = _viaje.split(",")
-        cod_parada = int(viaje[11])
-        if cod_parada in lista_paradas_contador.keys():
-            lista_paradas_contador[cod_parada][0] += 1
-    archivo_viajes.close()
-
-    return lista_paradas_contador
-
-
-def obtener_paradas_mas_vendidas(lista_paradas_contador):
-    # key = calle1, values = [contador, calle 2, codigo_parada, desviacion, cantidad_viajes]
-    lista_paradas_avenida = {}
-
-    # Hacer un diccionario donde la key sea la avenida
-    for parada in lista_paradas_contador.items():
-        nombre_avenida = parada[1][1]
-        cant_boletos_vendidos = parada[1][0]
-        calle_2 = parada[1][2]
-        cod_parada = parada[0]
-        if nombre_avenida in lista_paradas_avenida:
-            lista_paradas_avenida[nombre_avenida].append(
-                [cant_boletos_vendidos, calle_2, cod_parada, 0, 0]
-            )
-        else:
-            lista_paradas_avenida[nombre_avenida] = [
-                [cant_boletos_vendidos, calle_2, cod_parada, 0, 0]
-            ]
-
-    # Ordenar las paradas por cantidad de boletos vendidos(descendente) y quedarnos con las 15 primeras paradas por cada avenida
-    for avenida in lista_paradas_avenida.items():
-        paradas_en_orden = sorted(avenida[1], key=lambda x: x[0], reverse=True)
-        nombre_avenida = avenida[0]
-        lista_paradas_avenida[nombre_avenida] = paradas_en_orden[0:15]
-
-    return lista_paradas_avenida
-
-
-def obtener_horarios_teoricos(lista_paradas_avenida):
-    # key = codigo_parada, value = diccionario_parada
-    # diccionario_parada: key = cod_parada, values = [tipo_dia_teorico, horario_teorico, nombre_avenida, nombre_interseccion]
-    lista_horarios_teoricos_parada = {}
-
-    # Recorrer los horarios teóricos y quedarnos solo con aquellos que pertenezcan a las paradas relevantes
-    archivo_horarios_teoricos = open(RUTA_ARCHIVO_HORARIOS_TEORICOS)
-    horarios_teoricos = archivo_horarios_teoricos.readlines()
-    horarios_teoricos = horarios_teoricos[1:]
-    for _horario_teorico in horarios_teoricos:
-        horario_teorico = _horario_teorico.split(";")
-        salir = False
-        for avenida in lista_paradas_avenida.items():
-            paradas_por_avenida = avenida[1]
-            for parada in paradas_por_avenida:
-                cod_parada = int(horario_teorico[3])
-                if cod_parada == parada[2]:
-                    salir = True
-                    tipo_dia_teorico = int(horario_teorico[0])
-                    cod_variante = int(horario_teorico[1])
-                    horario_teorico_aux = int(horario_teorico[5])
-                    nombre_avenida = avenida[0]
-                    nombre_interseccion = parada[1]
-                    if cod_parada in lista_horarios_teoricos_parada:
-                        if cod_variante in lista_horarios_teoricos_parada[cod_parada]:
-                            lista_horarios_teoricos_parada[cod_parada][
-                                cod_variante
-                            ].append(
-                                [
-                                    tipo_dia_teorico,
-                                    horario_teorico_aux,
-                                    nombre_avenida,
-                                    nombre_interseccion,
-                                ]
-                            )
-                        else:
-                            lista_horarios_teoricos_parada[cod_parada][cod_variante] = [
-                                [
-                                    tipo_dia_teorico,
-                                    horario_teorico_aux,
-                                    nombre_avenida,
-                                    nombre_interseccion,
-                                ]
-                            ]
-                    else:
-                        lista_horarios_teoricos_parada[cod_parada] = {}
-                        lista_horarios_teoricos_parada[cod_parada][cod_variante] = [
-                            [
-                                tipo_dia_teorico,
-                                horario_teorico_aux,
-                                nombre_avenida,
-                                nombre_interseccion,
-                            ]
-                        ]
-                    break
-            if salir:
-                break
-    archivo_horarios_teoricos.close()
-
-    # Recorrer los horarios teóricos circulares y quedarnos solo con aquellos que pertenezcan a las paradas relevantes
-    archivo_horarios_teoricos_circulares = open(
-        RUTA_ARCHIVO_HORARIOS_TEORICOS_CIRCULARES
-    )
-    horarios_teoricos_circulares = archivo_horarios_teoricos_circulares.readlines()
-    horarios_teoricos_circulares = horarios_teoricos_circulares[1:]
-    for _horario_teorico_circular in horarios_teoricos_circulares:
-        horario_teorico = _horario_teorico_circular.split(";")
-        salir = False
-        for avenida in lista_paradas_avenida.items():
-            paradas_por_avenida = avenida[1]
-            for parada in paradas_por_avenida:
-                cod_parada = int(horario_teorico[3])
-                if cod_parada == parada[2]:
-                    salir = True
-                    tipo_dia_teorico = int(horario_teorico[0])
-                    cod_variante = int(horario_teorico[4])
-                    horario_teorico_aux = int(horario_teorico[7])
-                    nombre_avenida = avenida[0]
-                    nombre_interseccion = parada[1]
-                    if cod_parada in lista_horarios_teoricos_parada:
-                        if cod_variante in lista_horarios_teoricos_parada[cod_parada]:
-                            lista_horarios_teoricos_parada[cod_parada][
-                                cod_variante
-                            ].append(
-                                [
-                                    tipo_dia_teorico,
-                                    horario_teorico_aux,
-                                    nombre_avenida,
-                                    nombre_interseccion,
-                                ]
-                            )
-                        else:
-                            lista_horarios_teoricos_parada[cod_parada][cod_variante] = [
-                                [
-                                    tipo_dia_teorico,
-                                    horario_teorico_aux,
-                                    nombre_avenida,
-                                    nombre_interseccion,
-                                ]
-                            ]
-                    else:
-                        lista_horarios_teoricos_parada[cod_parada] = {}
-                        lista_horarios_teoricos_parada[cod_parada][cod_variante] = [
-                            [
-                                tipo_dia_teorico,
-                                horario_teorico_aux,
-                                nombre_avenida,
-                                nombre_interseccion,
-                            ]
-                        ]
-                    break
-        if salir:
-            break
-    archivo_horarios_teoricos.close()
-
-    return lista_horarios_teoricos_parada
-
-
-def calcular_frecuencia(lista_horarios_teoricos_parada):
-    # calcular la frecuencia para cada horario teórico
-    for parada in lista_horarios_teoricos_parada:
-        for variante in lista_horarios_teoricos_parada[parada]:
-            lista_horarios_teoricos_parada[parada][variante].sort()
-            horarios_teoricos_ordenados = sorted(
-                lista_horarios_teoricos_parada[parada][variante],
-                key=lambda x: x[1],
-            )
-            for index_horario, horario_teorico in enumerate(
-                horarios_teoricos_ordenados
-            ):
-                frecuencia = func.calcular_frecuencia(
-                    horario_teorico, index_horario, horarios_teoricos_ordenados
-                )
-                horario_teorico.append(frecuencia)
-
-
-def calcular_desviacion_por_dia_hora(viajes, lista_horarios_teoricos_parada, cola_res):
-    res_parcial = {}
-    for _viaje in viajes:
-        viaje = _viaje.split(",")
-        cod_parada = int(viaje[11])
-        cod_variante = int(viaje[16])
-        if cod_parada in lista_horarios_teoricos_parada:
-            if cod_variante in lista_horarios_teoricos_parada[cod_parada]:
-                for horario_teorico in lista_horarios_teoricos_parada[cod_parada][
-                    cod_variante
-                ]:
-                    frecuencia = horario_teorico[4]
-                    horario_real = viaje[2]
-                    desviacion = func.comparar_horarios(
-                        horario_real, horario_teorico, frecuencia, DEFAULT_MARGEN
-                    )
-                    if desviacion != None:
-                        nombre_interseccion = horario_teorico[3]
-                        dia_de_la_semana = func.obtener_dia_semana(horario_real)
-                        franja_horaria = func.obtener_franja_horaria(horario_real)
-                        nombre_avenida = horario_teorico[2]
-                        linea_empresa = viaje[13] + " " + viaje[15]
-                        res_parcial = func.agregar_desviacion_dia_hora(
-                            res_parcial,
-                            nombre_avenida,
-                            str(cod_parada) + " " + nombre_interseccion,
-                            desviacion,
-                            linea_empresa,
-                            1,
-                            dia_de_la_semana,
-                            franja_horaria,
-                        )
-    cola_res.put(res_parcial)
-
 
 if __name__ == "__main__":
     comienzo_tiempo = time.time()
     lista_avenidas = ["AV GRAL RIVERA"]
-    lista_paradas_contador = obtener_paradas_avenida(lista_avenidas)
-    lista_paradas_avenida = obtener_paradas_mas_vendidas(lista_paradas_contador)
+    lista_paradas_contador = func_v.obtener_paradas_avenida(lista_avenidas)
+    lista_paradas_avenida = func_v.obtener_paradas_mas_vendidas(
+        lista_paradas_contador)
     del lista_paradas_contador
-    lista_horarios_teoricos_parada = obtener_horarios_teoricos(lista_paradas_avenida)
+    lista_horarios_teoricos_parada = func_v.obtener_horarios_teoricos(
+        lista_paradas_avenida)
     del lista_paradas_avenida
-    calcular_frecuencia(lista_horarios_teoricos_parada)
+    func_v.calcular_frecuencias(lista_horarios_teoricos_parada)
 
     archivo_viajes = open(RUTA_ARCHIVO_VIAJES)
     viajes = archivo_viajes.readlines()
@@ -278,8 +33,9 @@ if __name__ == "__main__":
         )
         procesos.append(
             mp.Process(
-                target=calcular_desviacion_por_dia_hora,
-                args=(viajes[comienzo:fin], lista_horarios_teoricos_parada, cola_res),
+                target=func_v.calcular_desviacion_por_dia_hora,
+                args=(viajes[comienzo:fin],
+                      lista_horarios_teoricos_parada, cola_res),
             )
         )
 
@@ -305,7 +61,7 @@ if __name__ == "__main__":
                             linea_empresa_id = linea_empresa[0]
                             desviacion = linea_empresa[1][0]
                             cantidad_viajes = linea_empresa[1][1]
-                            resultado_final = func.agregar_desviacion_dia_hora(
+                            resultado_final = func_v.agregar_desviacion_dia_hora(
                                 resultado_final,
                                 nombre_avenida,
                                 cod_parada,
